05bedfd20f9025fd5a878077b74b748b3ee31550
docorlag <ronelll3malpica@gmail.com>
2020-03-11 06:35:10 -0500

    agregando fecha a la lista

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const  $idDate = document.getElementById('date');
 function  showDate(id){
+    const days = [  "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" ]
+    const month =["Sunday" ,"February","March","April","May","June","July" , "August","September" ,"October" , "November" ,"December"]
     let  fecha  = new Date();
-    id.innerHTML = `${fecha.getDay()}  ,${fecha.getMonth() } ${fecha.getDay() + 1} `
+    id.innerHTML = `${days[fecha.getDay() ]}  ,${ month[fecha.getMonth()] } ${fecha.getDay() + 1} `
 }
 showDate( $idDate )
